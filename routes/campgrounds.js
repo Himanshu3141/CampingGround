cd37a969f24d7a47a6a02dbcb7b517738cb359f2
HIMANSHU. <himanshu3141@gmail.com>
2024-11-30 15:10:09 +0530

    minor changes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -8,7 +8,7 @@ const isLoggedIn = require('../middleware');
 const multer = require('multer');
 const { storage } = require('../cloudinary');
 const upload = multer({ storage });
-const campgrounds = require('../controllers/campgrounds'); // Import the campgrounds controller
+const campgrounds = require('../controllers/campgrounds'); 
 
 const validateCampground = (req, res, next) => {
   const campgroundSchema = joi.object({
@@ -39,39 +39,37 @@ const isAuthor = async(req, res, next) => {
   next();
 }
 
-// Route for creating a campground with image upload
+
 router.route('/')
-  .get(catchAsync(campgrounds.index)) // Use the 'index' method from campgrounds controller
+  .get(catchAsync(campgrounds.index))
   .post(isLoggedIn, upload.single('image'), catchAsync(async (req, res) => {
-    // Ensure user is logged in
     if (!req.user) {
       req.flash('error', 'You must be logged in to create a campground.');
       return res.redirect('/login');
     }
 
     const { title, location, price, description } = req.body.campground;
-    const image = req.file.path;  // Cloudinary image URL
+    const image = req.file.path;  
 
-    // Create a new campground with the image URL and user ID
+
     const newCampground = new Campground({
       title,
       location,
       price,
       description,
-      image,  // Store the Cloudinary image URL
-      author: req.user._id,  // Use the logged-in user's ID
+      image,  
+      author: req.user._id,  
     });
 
-    // Save the campground to the database
+  
     await newCampground.save();
     
-    // Redirect to the newly created campground's page
+
     res.redirect(`/campgrounds/${newCampground._id}`);
   }));
 
 router.get('/new', isLoggedIn, campgrounds.rendernewform);
 
-// Route for editing an existing campground
 router.route('/:id')
   .get(catchAsync(campgrounds.showCampground))
   .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
